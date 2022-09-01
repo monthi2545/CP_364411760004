c7c8e92d0a2dd841fc14451b7eb802d0824d1ccd
mon <monyhira.j@rmutsvmail.com>
2022-09-01 17:14:28 +0700

    lab3 if-lese,loop

---
+++ Lab3/ex_for1.py
@@ -0,0 +1,27 @@
+"""
+Name : Monthira Jamikorn
+ID :364411760004
+Grop:Mit421
+"""
+
+# for loop
+
+# range()
+
+for x in range(10): #0-9
+    print(x,end="-")
+
+# 1-10
+for x in range(1,11):
+    print(x,end="-")
+
+# แสดงตัวเลขตั้งแต่ 1-100 ที่หารด้วย 5 ลงตัว
+
+
+for x in range(5, 101, 5):
+    print(x,end="-")
+
+# แสดงตัวเลขตั้งแต่ 1-100 ที่หารด้วย 3 และ 5 ลงตัว
+for x in range(1,101)
+    if x%3==0 and x%5==0:
+        print(x,end="-")
---
+++ Lab3/ex_ifeise2.py
@@ -0,0 +1,17 @@
+"""
+Name : Monthira Jamikorn
+ID :364411760004
+Grop:Mit421
+"""
+
+# one line if else statement,with 3 conditions
+
+a = 330
+b = 330
+
+if a>b:
+    print("A")
+elif a==b
+    print("=")
+else:
+    print("B")
